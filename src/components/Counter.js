ed4da62f67efe95afee5ae0f8c6e55a3e4b2d2d9
Seth Dimmer <sethdimmer1@gmail.com>
2023-04-19 19:13:09 -0400

    initial commit

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,8 +1,15 @@
 import { useState } from "react"
 import Counter2 from "./Counter2"
+import { useSelector } from "react-redux"
 
 export default function Counter() {
-    const [count, setCount] = useState(0)
+    // const [count, setCount] = useState(0)
+
+    const count = 0 //takes away error
+
+    const count2 = useSelector(state => {
+        console.log(state)
+    })
     return (
         <div>
             <h1> Count is {count}</h1>
