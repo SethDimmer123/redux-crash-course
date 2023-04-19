5ff3c60d92e18a5018f4f1eae33a130d3d56f81c
Seth Dimmer <sethdimmer1@gmail.com>
2023-04-19 18:14:05 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Counter from './components/Counter';
+import Counter2 from './components/Counter2';
+function App() {
+  return (
+    <div>
+      <Counter/>
+      <Counter2/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Counter2.js
@@ -0,0 +1,12 @@
+
+
+export default function Counter2() {
+
+    return (
+        <div>
+            <h1> This is Counter 2</h1>
+            <button>Increment</button>
+            <button>Decrement</button>
+        </div>
+    )
+}
