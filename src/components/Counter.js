2f5470f8c57c5c38a9b1c654873f85cba7822b7f
Seth Dimmer <sethdimmer1@gmail.com>
2023-04-19 22:41:51 -0400

    initial commit

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,6 +1,9 @@
 import { useState } from "react"
 import Counter2 from "./Counter2"
 import { useSelector } from "react-redux"
+import { useDispatch } from "react-redux"
+import { increment, incrementByAmount } from "../redux/counterSlice"
+import { decrement } from "../redux/counterSlice"
 
 export default function Counter() {
     // const [count, setCount] = useState(0)
@@ -11,15 +14,22 @@ export default function Counter() {
     //     console.log(state)
     //     return state.counter.count
     // })
-    
+
     const count =useSelector(state => state.counter.count)
+    const dispatch = useDispatch()
     return (
         <div>
             <h1> Count is {count}</h1>
-            <button>Increment</button>
+            <button
+            onClick={() => dispatch(increment()
+            )}>Increment</button>
 
 
-            <button>Decrement</button>
+            <button onClick={() => dispatch(decrement()
+            )}>Decrement</button>
+
+            <button onClick={() => dispatch(incrementByAmount(20)
+            )}>Increment By Amount</button>
         </div>
     )
 }
--- src/components/Counter2.js
+++ src/components/Counter2.js
@@ -1,12 +1,17 @@
-
+import { useState } from "react"
+import { useDispatch } from "react-redux"
+import { increment } from "../redux/counterSlice"
+import { decrement } from "../redux/counterSlice"
 
 export default function Counter2() {
+
+    const dispatch = useDispatch()
 
     return (
         <div>
             <h1> This is Counter 2</h1>
-            <button>Increment</button>
-            <button>Decrement</button>
+            <button onClick={() => dispatch(increment())}>Increment</button>
+            <button onClick={() => dispatch(decrement())}>Decrement</button>
         </div>
     )
 }
