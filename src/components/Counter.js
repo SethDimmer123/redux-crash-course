753ea45ed4d15e8c929783fb5ec798a2b598a0fb
Seth Dimmer <sethdimmer1@gmail.com>
2023-04-19 23:51:39 -0400

    initial commit

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,7 +4,7 @@ import { useSelector } from "react-redux"
 import { useDispatch } from "react-redux"
 import { increment, incrementByAmount } from "../redux/counterSlice"
 import { decrement } from "../redux/counterSlice"
-
+import { useGetAllPostsQuery,useGetPostbyIdQuery } from "../redux/postsSlice"
 export default function Counter() {
     // const [count, setCount] = useState(0)
 
@@ -15,14 +15,21 @@ export default function Counter() {
     //     return state.counter.count
     // })
 
-    const count =useSelector(state => state.counter.count)
+    const count = useSelector(state => state.counter.count)
     const dispatch = useDispatch()
+
+    // console.log(useGetAllPostsQuery())
+
+    // const {data,isLoading, isError} = useGetAllPostsQuery()
+    const {data,isLoading, isError} = useGetPostbyIdQuery(1)
+
     return (
         <div>
+            {JSON.stringify(data)}
             <h1> Count is {count}</h1>
             <button
-            onClick={() => dispatch(increment()
-            )}>Increment</button>
+                onClick={() => dispatch(increment()
+                )}>Increment</button>
 
 
             <button onClick={() => dispatch(decrement()
--- src/redux/postsSlice.js
+++ src/redux/postsSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 // i can now use thse functions that i have imported.
 
-const postApi = createApi({
+ export const postsApi = createApi({
     reducerPath:"postApi",
     baseQuery:fetchBaseQuery({
         baseUrl: "https://jsonplaceholder.typicode.com/",
@@ -9,11 +9,20 @@ const postApi = createApi({
     endpoints:(bulider) => ({
         getAllPosts:bulider.query({
             query:() => "posts"
+        }),
+        getPostbyId:bulider.query({
+            query:(id) => `posts/${id}`,
         })
+        
     })
 });
-
+export const {useGetAllPostsQuery,useGetPostbyIdQuery} = postsApi//created byRTK QUERY
 // taking baseUrl and fetching posts
 
 // i have now created my api and i can use it in other components
 
+// how do i use it in other components?
+
+//  i need to export them add "export" to const postApi
+
+// also need to export another thing
